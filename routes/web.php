b4eb8634b3886c9b62b88ea41000616aa1da1e81
Senan Qulamov <qulamovsenan@gmail.com>
2025-11-12 14:14:04 +0400

    Initial clean commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Livewire\User\Profile;
+use Illuminate\Support\Facades\Route;
+use App\Livewire\Users\Index;
+
+Route::view('/', 'welcome')->name('welcome');
+
+Route::middleware(['auth'])->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+
+    Route::get('/users', Index::class)->name('users.index');
+
+    Route::get('/user/profile', Profile::class)->name('user.profile');
+});
+
+require __DIR__.'/auth.php';
